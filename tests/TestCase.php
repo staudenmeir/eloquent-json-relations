ef6934d2a0330218775033286a573637c98377ba
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2018-12-17 03:31:17 +0100

    Improve database configuration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,21 +18,10 @@ abstract class TestCase extends Base
     {
         parent::setUp();
 
+        $config = require __DIR__.'/config/database.php';
+
         $db = new DB;
-        $db->addConnection([
-            'driver' => 'mysql',
-            'host' => '127.0.0.1',
-            'port' => '3306',
-            'database' => 'test',
-            'username' => 'root',
-            'password' => '',
-            'unix_socket' => '',
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ]);
+        $db->addConnection($config[getenv('DB') ?: 'mysql']);
         $db->setAsGlobal();
         $db->bootEloquent();
 
