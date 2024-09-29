1f55c0dd282dd3b53c329f77389aa93b8cfdf302
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2024-09-29 14:54:21 +0200

    Increase PHPStan level

--- src/IdeHelperServiceProvider.php
+++ src/IdeHelperServiceProvider.php
@@ -11,20 +11,20 @@ class IdeHelperServiceProvider extends ServiceProvider implements DeferrableProv
 {
     public function register(): void
     {
-        /** @var \Illuminate\Contracts\Config\Repository $config */
+        /** @var \Illuminate\Config\Repository $config */
         $config = $this->app->get('config');
 
         $config->set(
             'ide-helper.model_hooks',
             array_merge(
                 [JsonRelationsHook::class],
-                (array) $config->get('ide-helper.model_hooks', [])
+                $config->array('ide-helper.model_hooks', [])
             )
         );
     }
 
     /**
-     * @return list<class-string>
+     * @return list<class-string<\Illuminate\Console\Command>>
      */
     public function provides(): array
     {
